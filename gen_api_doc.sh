761a809fca4698423b4de008dc21db69082b37a0
Jakub Mladý <scrub@students.zcu.cz>
2024-11-12 14:21:09 +0100

    move web to subdir

--- gen_api_doc.sh
+++ gen_api_doc.sh
@@ -1,1 +1,1 @@
-php ./Composer/vendor/bin/openapi -b Controllers/Api.class.php Controllers/Api.class.php -o api-doc.yaml
+php ./web/Composer/vendor/bin/openapi -b web/Controllers/Api.class.php Controllers/Api.class.php -o web/api-doc.yaml
