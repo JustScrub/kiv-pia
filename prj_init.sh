f272d644a1d0a0fdca36c929710a9701274796d2
Jakub Mladý <scrub@students.zcu.cz>
2025-01-09 13:55:53 +0100

    wf fix

--- prj_init.sh
+++ prj_init.sh
@@ -1,4 +1,4 @@
 docker run --rm --volume $PWD/web/Composer:/app composer install
 docker compose up -d
 WEB_UID=$(docker compose exec --no-TTY --user www-data pia_web id -u)
-chown -R $WEB_UID:$WEB_UID web/Articles
+chown -R $WEB_UID:$WEB_UID web/Articles || chmod -R 777 web/Articles
