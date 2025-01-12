d3ab0e469bd2f9f31764611555806a3befbc9e58
Jakub Mladý <scrub@students.zcu.cz>
2025-01-12 12:06:16 +0100

    easy mock/test DB data

--- SQL_Scripts/admin_mock_data.sql
+++ SQL_Scripts/admin_mock_data.sql
@@ -1,5 +1,6 @@
-insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Ansell', 'Floyd', 'afloyd0@delicious.com', 1, 'afloyd0', 'b6ecfa7353f96fb69a319d085ce4951cb9c1c5cc');
-insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Lucia', 'Elgood', 'lelgood1@uol.com.br', 2, 'lelgood1', 'b6ecfa7353f96fb69a319d085ce4951cb9c1c5cc');
-insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Denny', 'Thebeau', 'dthebeau2@guardian.co.uk', 2, 'dthebeau2', 'b6ecfa7353f96fb69a319d085ce4951cb9c1c5cc');
-insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Durand', 'Millson', 'dmillson3@zdnet.com', 2, 'dmillson3', 'b6ecfa7353f96fb69a319d085ce4951cb9c1c5cc');
-insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Cozmo', 'Leeuwerink', 'cleeuwerink4@facebook.com', 2, 'cleeuwerink4', 'b6ecfa7353f96fb69a319d085ce4951cb9c1c5cc');
+-- password: test
+insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Ansell', 'Floyd', 'afloyd0@delicious.com', 1, 'afloyd0', '$2y$10$1ogze8kbzvxfCtUcfqbcuujt6gkJJm46EXci9fRAAZb9v6zNEMW1y');
+insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Lucia', 'Elgood', 'lelgood1@uol.com.br', 2, 'lelgood1', '$2y$10$1ogze8kbzvxfCtUcfqbcuujt6gkJJm46EXci9fRAAZb9v6zNEMW1y');
+insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Denny', 'Thebeau', 'dthebeau2@guardian.co.uk', 2, 'dthebeau2', '$2y$10$1ogze8kbzvxfCtUcfqbcuujt6gkJJm46EXci9fRAAZb9v6zNEMW1y');
+insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Durand', 'Millson', 'dmillson3@zdnet.com', 2, 'dmillson3', '$2y$10$1ogze8kbzvxfCtUcfqbcuujt6gkJJm46EXci9fRAAZb9v6zNEMW1y');
+insert into uzivatel (jmeno, prijmeni, email, id_pravo, login, heslo) values ('Cozmo', 'Leeuwerink', 'cleeuwerink4@facebook.com', 2, 'cleeuwerink4', '$2y$10$1ogze8kbzvxfCtUcfqbcuujt6gkJJm46EXci9fRAAZb9v6zNEMW1y');
--- prj_init.sh
+++ prj_init.sh
@@ -1,3 +1,26 @@
+#!/bin/bash
+# modify compose.yml based on flags
+# -a: add admin mock data, -u: add user mock data, -t: add test data
+
+while getopts "aut" opt; do
+  case $opt in
+    a)
+      sed -i "s|#ADMINMOCKDATA|- ./SQL_Scripts/admin_mock_data.sql:/docker-entrypoint-initdb.d/04.sql|" compose.yaml 
+      ;;
+    u)
+      sed -i "s|#USERMOCKDATA|- ./SQL_Scripts/uzivatel_mock_data.sql:/docker-entrypoint-initdb.d/03.sql|" compose.yaml 
+      ;;
+    t)
+      sed -i "s|#APITESTSQLSCRIPT|- ./SQL_Scripts/api_test_data.sql:/docker-entrypoint-initdb.d/02.sql|" compose.yaml 
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+
+
 docker run --rm --volume $PWD/web/Composer:/app composer install
 docker compose up -d
 WEB_UID=$(docker compose exec --no-TTY --user www-data pia_web id -u)
